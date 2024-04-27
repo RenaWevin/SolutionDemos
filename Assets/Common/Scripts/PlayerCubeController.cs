525acb4bf9e69a07d9b4e65532f1bce284bf4eaf
RenaWevin <renawevin@gmail.com>
2024-04-27 20:44:37 +0800

    [+] 完成範例: SaveGameInPlayerPrefs

---
+++ Assets/Common/Scripts/PlayerCubeController.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+
+namespace Common {
+    public class PlayerCubeController : MonoBehaviour {
+
+        [SerializeField] private Transform transformToMove;
+        [SerializeField] private UpMovement upIs;
+        [SerializeField] private float moveSpeed = 10f;
+
+        private enum UpMovement {
+            PositiveY,
+            PositiveZ,
+        }
+        
+        void FixedUpdate() {
+            Vector3 upMovement = upIs == UpMovement.PositiveY ? Vector3.up : Vector3.forward;
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                //上
+                Move(upMovement * moveSpeed * Time.fixedDeltaTime);
+            } else if (Input.GetKey(KeyCode.DownArrow)) {
+                //下
+                Move(-upMovement * moveSpeed * Time.fixedDeltaTime);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow)) {
+                //左
+                Move(Vector3.left * moveSpeed * Time.fixedDeltaTime);
+            } else if (Input.GetKey(KeyCode.RightArrow)) {
+                //右
+                Move(Vector3.right * moveSpeed * Time.fixedDeltaTime);
+            }
+        }
+
+        private void Move(Vector3 movement) {
+            if (transformToMove != null) {
+                transformToMove.Translate(movement);
+            }
+        }
+    }
+}
---
+++ Assets/Solution-SaveGameInPlayerPrefs/SaveGameInPlayerPrefs.cs
@@ -0,0 +1,61 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Solution.SaveGameInPlayerPrefs {
+
+    public class SaveGameInPlayerPrefs : MonoBehaviour {
+
+        //玩家本體Transform
+        [SerializeField] private Transform player;
+
+        //現在位置
+        [SerializeField] private Text text_Position;
+        //存讀檔按鈕
+        [SerializeField] private Button btn_Save;
+        [SerializeField] private Button btn_Load;
+        //存讀檔訊息
+        [SerializeField] private Text text_SaveLoadInfo;
+
+        private const string Key_PlayerPositionX = "PlayerPositionX";
+        private const string Key_PlayerPositionY = "PlayerPositionY";
+        private const string Key_PlayerPositionZ = "PlayerPositionZ";
+
+        void Start() {
+            btn_Save.onClick.RemoveAllListeners();
+            btn_Save.onClick.AddListener(SaveGame);
+            btn_Load.onClick.RemoveAllListeners();
+            btn_Load.onClick.AddListener(LoadGame);
+        }
+
+        void Update() {
+            Vector3 pos = player.position;
+            text_Position.text = $"X: {pos.x:F2}\nY: {pos.y:F2}\nZ: {pos.z:F2}";
+        }
+
+        /// <summary>
+        /// 存檔
+        /// </summary>
+        private void SaveGame() {
+            Vector3 pos = player.position;
+            PlayerPrefs.SetFloat(Key_PlayerPositionX, pos.x);
+            PlayerPrefs.SetFloat(Key_PlayerPositionY, pos.y);
+            PlayerPrefs.SetFloat(Key_PlayerPositionZ, pos.z);
+            PlayerPrefs.Save();
+            text_SaveLoadInfo.text = $"[{System.DateTime.Now}] 存檔完成";
+        }
+
+        /// <summary>
+        /// 讀檔
+        /// </summary>
+        private void LoadGame() {
+            Vector3 pos;
+            pos.x = PlayerPrefs.GetFloat(Key_PlayerPositionX, 0f);
+            pos.y = PlayerPrefs.GetFloat(Key_PlayerPositionY, 0f);
+            pos.z = PlayerPrefs.GetFloat(Key_PlayerPositionZ, 0f);
+            player.position = pos;
+            text_SaveLoadInfo.text = $"[{System.DateTime.Now}] 讀檔完成";
+        }
+
+    }
+}
