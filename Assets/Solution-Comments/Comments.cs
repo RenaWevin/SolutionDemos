0bc57cfb346ff239a7ae3af61ca1c00ccc7cc3f5
RenaWevin <renawevin@gmail.com>
2024-04-27 23:58:12 +0800

    [+] 新增範例: Comments

---
+++ Assets/Solution-Comments/Comments.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+
+namespace Solution.Comments {
+
+    public class Comments : MonoBehaviour {
+
+        /// <summary>
+        /// 程式起始點
+        /// </summary>
+        void Start() {
+            //取得物件位置
+            var pos = GetPosition(this.gameObject);
+            //轉換成戰鬥點數
+            var battlePoint = ConvertPositionToBattlePoints(pos);
+            //輸出結果
+            Debug.Log("BP: " + battlePoint.ToString());
+        }
+
+        /// <summary>
+        /// 取得遊戲物件的位置
+        /// </summary>
+        /// <returns></returns>
+        private Vector3 GetPosition(GameObject targetGO) {
+            var targetTransform = targetGO.GetComponent<Transform>();
+            return (targetTransform = null) ? Vector3.zero : targetTransform.position;
+        }
+
+        /// <summary>
+        /// 透過位置計算該座標會有的戰鬥點數，XYZ參數計算後取最大值
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        private float ConvertPositionToBattlePoints(Vector3 position) {
+            //X的絕對值
+            float battleX = Mathf.Abs(position.x);
+            //(Y^3次方 除以 20)取整數
+            float battleY = Mathf.Ceil(Mathf.Pow(position.y, 3) / 20f);
+            //選擇下面兩個其中一個的最大值：(根號Z 乘 Log(Z) 乘 10的5次方) 以及 (Z的2次方)
+            float battleZ = Mathf.Max(Mathf.Sqrt(position.z) * Mathf.Log10(position.z) * Mathf.Pow(10, 5), Mathf.Pow(position.z, 2));
+            return Mathf.Max(battleX, battleY, battleZ);
+        }
+
+    }
+
+}
+
---
+++ Assets/Solution-Comments/NoComments.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+namespace Solution.Comments {
+
+    public class NoComments : MonoBehaviour {
+
+        void Start() {
+            var pos = GetPos(this.gameObject);
+            var b = ConvertP2BP(pos);
+            Debug.Log("BP: " + b.ToString());
+        }
+
+        private Vector3 GetPos(GameObject GO) {
+            var trans = GO.GetComponent<Transform>();
+            return (trans = null) ? Vector3.zero : trans.position;
+        }
+
+        private float ConvertP2BP(Vector3 p) {
+            float b1 = Mathf.Abs(p.x);
+            float b2 = Mathf.Ceil(Mathf.Pow(p.y, 3) / 20f);
+            float b3 = Mathf.Max(Mathf.Sqrt(p.z) * Mathf.Log10(p.z) * Mathf.Pow(10, 5), Mathf.Pow(p.z, 2));
+            return Mathf.Max(b1, b2, b3);
+        }
+
+    }
+
+}
+
