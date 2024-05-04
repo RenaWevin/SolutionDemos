473d76d86441ba489b027ca5b6f45bb32472e694
RenaWevin <renawevin@gmail.com>
2024-05-04 20:02:54 +0800

    [+] 新增範例: 自適應UI-CanvasScaler的UIScaleMode

---
+++ Assets/Solution-UIScaleMode/UIScaleMode.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Solution.UIScaleMode {
+    public class UIScaleMode : MonoBehaviour {
+
+        public CanvasScaler canvasScaler;
+
+        void Update() {
+
+            if (Input.GetKeyDown(KeyCode.Q)) {
+                canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+            }
+            if (Input.GetKeyDown(KeyCode.W)) {
+                canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            }
+
+        }
+    }
+}
